a2efe1465bc54c16d47e694659045d117f920933
tausifcoder1111 <tausifcoderr@gmail.com>
2025-03-20 21:13:39 +0530

    Prepare for Netlify deployment"

--- tausif-portfolio/vite.config.js
+++ tausif-portfolio/vite.config.js
@@ -5,8 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  root: '.',
-  base :"/Tausif-Portfolio",
   build: {
     outDir: "dist", // Ensure Vite outputs the build files to 'dist'
     rollupOptions: {
